d9cd669bb9c5a893d3b7bf188808875009a1ffb7
azarasi-0925 <azmodder.japan@gmail.com>
2022-07-08 08:42:07 +0900

    a

--- index.js
+++ index.js
@@ -13,42 +13,55 @@ window.onload = function (e) {
 function sendMessage(){
     liff.sendMessages([
         {
-            "type": "bubble",
-            "body": {
-              "type": "box",
-              "layout": "vertical",
-              "contents": [
-                {
-                  "type": "image",
-                  "url": "https://www.silhouette-illust.com/wp-content/uploads/2017/08/icon_movie_36335-300x300.jpg"
-                },
-                {
-                  "type": "separator"
-                },
-                {
-                  "type": "text",
-                  "text": "Text in the box"
-                },
-                {
+            "type": "flex",
+            "altText": "flexMessageです",
+            "contents": {
+                "type": "bubble",
+                "body": {
                   "type": "box",
                   "layout": "vertical",
-                  "contents": [],
-                  "width": "30px",
-                  "height": "30px",
-                  "background": {
-                    "type": "linearGradient",
-                    "angle": "90deg",
-                    "startColor": "#FFFF00",
-                    "endColor": "#0080ff"
+                  "contents": [
+                    {
+                      "type": "text",
+                      "text": "APENGテスト",
+                      "weight": "bold",
+                      "size": "xl"
+                    },
+                    {
+                      "type": "text",
+                      "text": "電話をかけています..."
+                    },
+                    {
+                      "type": "image",
+                      "url": "https://qiita-image-store.s3.ap-northeast-1.amazonaws.com/0/488939/b9b1f3c9-b8bc-eaa4-5d75-4868ee3f1fbd.png",
+                      "animated": true
+                    }
+                  ]
+                },
+                "footer": {
+                  "type": "box",
+                  "layout": "vertical",
+                  "contents": [
+                    {
+                      "type": "text",
+                      "text": "twitter:@inoue2002",
+                      "weight": "bold",
+                      "style": "normal",
+                      "decoration": "none",
+                      "position": "relative",
+                      "align": "center"
+                    }
+                  ]
+                },
+                "styles": {
+                  "footer": {
+                    "backgroundColor": "#d4d9df",
+                    "separator": true,
+                    "separatorColor": "#d4d9df"
                   }
                 }
-              ],
-              "height": "400px",
-              "justifyContent": "space-evenly",
-              "alignItems": "center"
-            }
-          }
-          ,
+              }
+          },
     ]).then(function () {
         liff.closeWindow();
     }).catch(function (error) {

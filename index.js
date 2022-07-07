7bc97c8aab0395d0204ef307e94ccebdd3c2a45f
azarasi-0925 <azmodder.japan@gmail.com>
2022-07-08 08:45:31 +0900

    a

--- index.js
+++ index.js
@@ -18,50 +18,12 @@ function sendMessage(){
             "contents": {
                 "type": "bubble",
                 "body": {
-                  "type": "box",
-                  "layout": "vertical",
-                  "contents": [
-                    {
-                      "type": "text",
-                      "text": "APENGテスト",
-                      "weight": "bold",
-                      "size": "xl"
-                    },
-                    {
-                      "type": "text",
-                      "text": "電話をかけています..."
-                    },
-                    {
-                      "type": "image",
-                      "url": "https://qiita-image-store.s3.ap-northeast-1.amazonaws.com/0/488939/b9b1f3c9-b8bc-eaa4-5d75-4868ee3f1fbd.png",
-                      "animated": true
-                    }
-                  ]
-                },
-                "footer": {
-                  "type": "box",
-                  "layout": "vertical",
-                  "contents": [
-                    {
-                      "type": "text",
-                      "text": "twitter:@inoue2002",
-                      "weight": "bold",
-                      "style": "normal",
-                      "decoration": "none",
-                      "position": "relative",
-                      "align": "center"
-                    }
-                  ]
-                },
-                "styles": {
-                  "footer": {
-                    "backgroundColor": "#d4d9df",
-                    "separator": true,
-                    "separatorColor": "#d4d9df"
-                  }
-                }
+                    "type": "image",
+                    "url": "https://qiita-image-store.s3.ap-northeast-1.amazonaws.com/0/488939/b9b1f3c9-b8bc-eaa4-5d75-4868ee3f1fbd.png",
+                    "animated": true
               }
-          },
+            }
+        },
     ]).then(function () {
         liff.closeWindow();
     }).catch(function (error) {

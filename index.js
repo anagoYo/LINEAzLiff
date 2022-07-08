5d45835e34f7df9dce01cb9322e369d91daf5272
azarasi-0925 <azmodder.japan@gmail.com>
2022-07-08 09:13:47 +0900

    a

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function sendMessage(){
     liff.sendMessages([
         {
             "type": "flex",
-            "altText": "this is a flex message",
+            "altText": "AzSticker",
             "contents": {
               "type": "bubble",
               "body": {
@@ -30,11 +30,39 @@ function sendMessage(){
                 ]
               }
             }
-          }
-          ,
+          },
     ]).then(function () {
         liff.closeWindow();
     }).catch(function (error) {
         window.alert('Error sending message: ' + error.message);
     });
+}
+
+function share(){
+    liff.shareTargetPicker([
+        {
+            "type": "flex",
+            "altText": "AzSticker",
+            "contents": {
+              "type": "bubble",
+              "body": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                    {
+                        "type": "image",
+                        "url": "https://stickershop.line-scdn.net/products/0/0/1/19134836/android/animation/492435199.png",
+                        "size": "full",
+                        "animated": true
+                    }
+                    ]
+                }
+            }
+        },
+    ])
+    .then(
+        alert("ShareTargetPicker was launched")
+    ).catch(function(res) {
+        alert("Failed to launch ShareTargetPicker")
+    })
 }

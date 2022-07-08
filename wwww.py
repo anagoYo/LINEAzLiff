9113b9650df03a75ad5081d1ac50636c873140cc
azarasi-0925 <azmodder.japan@gmail.com>
2022-07-08 09:49:40 +0900

    a

--- wwww.py
+++ wwww.py
@@ -4,10 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", message="https://liff.line.me/0000-xxxx/パッケージID")
-
-@app.route("/sticker")
-def sticker():
     return render_template("sticker.html", packageId=request.args)
 
 if __name__ == "__main__":

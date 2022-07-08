6b44cf557408635e97b13bdca4735171405e24db
azarasi-0925 <azmodder.japan@gmail.com>
2022-07-08 09:06:54 +0900

    a

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function sendMessage(){
                 "contents": [
                   {
                     "type": "image",
-                    "url": "https://stickershop.line-scdn.net/products/0/0/1/19134836/android/stickers/492435199.png",
+                    "url": "https://stickershop.line-scdn.net/products/0/0/1/19134836/android/animation/492435199.png",
                     "animated": true
                   }
                 ]

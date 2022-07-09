5dd1fe35324973042e5db17e179c5d3bb3d7941f
anago <85058870+anagoYo@users.noreply.github.com>
2022-07-09 09:54:14 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ window.onload = function (e) {
     liff.init({
         liffId: '1657284809-ew3YylaE'
     }).then(() => {
-        if (!liff.isInClient()) {
-            window.alert('This button is unavailable as LIFF is currently being opened in an external browser.');
-        } else {
-            
+        if(liff.isLoggedIn()){
+            liff.getProfile()
+        }else{
+            liff.login()
         }
     });
 };

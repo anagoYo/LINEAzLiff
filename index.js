51be3af6860d842d488489b237f4dd3466571df1
anago <85058870+anagoYo@users.noreply.github.com>
2022-07-12 20:12:49 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require("Utils.js");
+
 window.onload = function (e) {
     liff.init({
         liffId: '1657284809-ew3YylaE'
@@ -10,94 +12,7 @@ window.onload = function (e) {
     });
 };
 
-function bool(data) {
-    return String(data).toLowerCase() === "true";
-}
-
-function getLocalInt(_key, _default){
-    const item = localStorage.getItem(_key);
-    if(item){
-        return parseInt(item);
-    }
-    return parseInt(_default)
-}
-
-function getLocalBool(_key, _default){
-    const item = localStorage.getItem(_key);
-    if(item){
-        return bool(item);
-    }
-    return bool(_default)
-}
-
-function getLocalString(_key, _default){
-    const item = localStorage.getItem(_key);
-    if(item){
-        return item;
-    }
-    return _default
-}
-
-function getParam(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, "\\$&");
-    let regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
-}
-
-function is404(_url){
-    let xhr;
-    try{
-        xhr = new XMLHttpRequest();
-        xhr.open("HEAD", _url, false);
-        xhr.send(null);
-    } catch (error) {
-        return true;
-    }
-    if(xhr == null){
-        return true;
-    }
-    return xhr.status == 404;
-}
-
-function getSizeString(sizeNum){
-    if(String(sizeNum) == "0"){
-        return "xxs"
-    }
-    if(String(sizeNum) == "1"){
-        return "xs"
-    }
-    if(String(sizeNum) == "2"){
-        return "sm"
-    }
-    if(String(sizeNum) == "3"){
-        return "md"
-    }
-    if(String(sizeNum) == "4"){
-        return "lg"
-    }
-    if(String(sizeNum) == "5"){
-        return "xl"
-    }
-    if(String(sizeNum) == "6"){
-        return "xxl"
-    }
-    if(String(sizeNum) == "7"){
-        return "3xl"
-    }
-    if(String(sizeNum) == "8"){
-        return "4xl"
-    }
-    if(String(sizeNum) == "9"){
-        return "5xl"
-    }
-    if(String(sizeNum) == "10"){
-        return "full"
-    }
-}
+const STICKERSHOP_ENDPOINT = "https://stickershop.line-scdn.net/products/0/0/1/"
 
 function share(json){
     liff.shareTargetPicker([
@@ -124,27 +39,27 @@ function sendSticker(packageId, stickerId){
                 }
             }
         }
-    }
+    };
 
     jsonData["contents"]["hero"]["size"] = getSizeString(getLocalInt("sticker_size", 0));
     jsonData["contents"]["hero"]["backgroundColor"] = getLocalString("sticker_backgroundColor", "#FFFFFF");
     jsonData["contents"]["hero"]["animated"] = getLocalBool("sticker_animated", false);
 
-    jsonData["contents"]["hero"]["url"] = "https://stickershop.line-scdn.net/products/0/0/1/"+packageId+"/android/"+(jsonData["contents"]["hero"]["animated"] ? "animation" : "stickers")+"/"+stickerId+".png";
+    jsonData["contents"]["hero"]["url"] = STICKERSHOP_ENDPOINT+packageId+"/android/"+(jsonData["contents"]["hero"]["animated"] ? "animation" : "stickers")+"/"+stickerId+".png";
     if(jsonData["contents"]["hero"]["animated"] && is404(jsonData["contents"]["hero"]["url"])){
-        jsonData["contents"]["hero"]["url"] = "https://stickershop.line-scdn.net/products/0/0/1/"+packageId+"/android/stickers/"+stickerId+".png";
+        jsonData["contents"]["hero"]["url"] = STICKERSHOP_ENDPOINT+packageId+"/android/stickers/"+stickerId+".png";
     }
 
     share(jsonData);
 }
 
-function select(){
+function input_packageId(){
     let packageId = window.prompt("パッケージIDを入力してください。", "");
     if(packageId.length < 2){
         return;
     }
-    fetch("https://stickershop.line-scdn.net/products/0/0/1/"+packageId+"/android/productInfo.meta").then((response) => {
-        return response.json()
+    fetch(STICKERSHOP_ENDPOINT+packageId+"/android/productInfo.meta").then((response) => {
+        return response.json();
     })
     .then((result) => {
         for (let sticker in result["stickers"]){
@@ -152,7 +67,7 @@ function select(){
             let stickerId = result["stickers"][sticker]["id"];
             const img = document.createElement("img");
             img.id = stickerId;
-            img.src = "https://stickershop.line-scdn.net/products/0/0/1/"+packageId+"/android/stickers/"+stickerId+".png";
+            img.src = STICKERSHOP_ENDPOINT+packageId+"/android/stickers/"+stickerId+".png";
 
             document.getElementById("stickers").appendChild(img);
 

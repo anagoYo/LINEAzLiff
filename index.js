42ea1446ee133e950bf4d599fc00e60c0ba31d22
anago <85058870+anagoYo@users.noreply.github.com>
2022-07-12 11:12:56 +0900

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,34 @@ window.onload = function (e) {
     });
 };
 
+function bool(data) {
+    return data.toLowerCase() === "true";
+}
+
+function getLocalInt(_key, _default){
+    const item = localStorage.getItem(_key);
+    if(item){
+        return parseInt(item);
+    }
+    return parseInt(_default)
+}
+
+function getLocalBool(_key, _default){
+    const item = localStorage.getItem(_key);
+    if(item){
+        return bool(item);
+    }
+    return bool(_default)
+}
+
+function getLocalString(_key, _default){
+    const item = localStorage.getItem(_key);
+    if(item){
+        return item;
+    }
+    return _default
+}
+
 function getParam(name, url) {
     if (!url) url = window.location.href;
     name = name.replace(/[\[\]]/g, "\\$&");
@@ -20,81 +48,78 @@ function getParam(name, url) {
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
-function share(type, packageId, stickerId){
-    if(type == null){
-        type = getParam("type")
-        if(type == null){
-            type = "static"   
-        }
+function is404(_url){
+    var xhr;
+    xhr = new XMLHttpRequest();
+    xhr.open("HEAD", _url, false);
+    xhr.send(null);
+    return xhr.status == 404;
+}
+
+function getSizeString(sizeNum){
+    if(String(sizeNum) == "0"){
+        return "xxs"
     }
-    if(packageId == null){
-        packageId = getParam("packageId")
-        if(packageId == null){
-            return;
-        }
+    if(String(sizeNum) == "1"){
+        return "xs"
     }
-    if(stickerId == null){
-        stickerId = getParam("stickerId")
-        if(stickerId == null){
-            return;
-        }
+    if(String(sizeNum) == "2"){
+        return "sm"
     }
-    var size = getParam("size")
-    if(size == null){
-        size = "full"
+    if(String(sizeNum) == "3"){
+        return "md"
     }
-    if(size == "1"){
-        size = "xl"
+    if(String(sizeNum) == "4"){
+        return "lg"
     }
-    if(size == "2"){
-        size = "xxl"
+    if(String(sizeNum) == "5"){
+        return "xl"
     }
-    if(size == "3"){
-        size = "3xl"
+    if(String(sizeNum) == "6"){
+        return "xxl"
     }
-    if(size == "4"){
-        size = "4xl"
+    if(String(sizeNum) == "7"){
+        return "3xl"
     }
-    if(size == "5"){
-        size = "5xl"
+    if(String(sizeNum) == "8"){
+        return "4xl"
     }
-    if(size == "6"){
-        size = "full"
+    if(String(sizeNum) == "9"){
+        return "5xl"
     }
-    var backgroundColor = getParam("backgroundColor")
-    if (backgroundColor == null){
-        backgroundColor = "FFFFFFFF"
+    if(String(sizeNum) == "10"){
+        return "full"
     }
-    var url = "";
-    var animated = false
-    if(type == "static"){
-        url = "https://stickershop.line-scdn.net/products/0/0/1/"+packageId+"/android/stickers/"+stickerId+".png"
-    }else if(type == "animation"){
-        animated = true;
-        url = "https://stickershop.line-scdn.net/products/0/0/1/"+packageId+"/android/animation/"+stickerId+".png"
-    }else{
-        url = "https://stickershop.line-scdn.net/products/0/0/1/"+packageId+"/android/stickers/"+stickerId+".png"
-    }
-    liff.shareTargetPicker([
-        {
-            "type": "flex",
-            "altText": "Azarasi Big Sticker",
-            "contents": {
-              "type": "bubble",
-              "hero": {
-                    "type": "image",
-                    "url": url,
-                    "size": size,
-                    "animated": animated,
-                    "backgroundColor": "#"+backgroundColor,
-                    "action": {
-                        "type": "uri",
-                        "uri": "https://line.me/R/shop/sticker/detail/"+packageId,
-                        "label": "shop"
-                    }
+}
+
+function share(packageId, stickerId){
+    let jsonData = {
+        "type": "flex",
+        "altText": "Azarasi Big Sticker",
+        "contents": {
+          "type": "bubble",
+          "hero": {
+                "type": "image",
+                "action": {
+                    "type": "uri",
+                    "uri": "https://line.me/R/shop/sticker/detail/"+packageId,
+                    "label": "shop"
                 }
             }
-        },
+        }
+    }
+
+    jsonData["contents"]["hero"]["size"] = getSizeString(getLocalInt("sticker_size", 0))
+    jsonData["contents"]["hero"]["backgroundColor"] = getLocalString("sticker_backgroundColor", "#FFFFFF")
+    jsonData["contents"]["hero"]["animated"] = getLocalBool("sticker_animated", false)
+
+    jsonData["contents"]["hero"]["url"] = "https://stickershop.line-scdn.net/products/0/0/1/"+packageId+"/android/"+(jsonData["contents"]["hero"]["animated"] ? "animation" : "stickers")+"/"+stickerId+".png"
+    if(is404(jsonData["contents"]["hero"]["url"])){
+        jsonData["contents"]["hero"]["url"] = "https://stickershop.line-scdn.net/products/0/0/1/"+packageId+"/android/stickers/"+stickerId+".png"
+    }
+
+    liff.shareTargetPicker([
+        jsonData
     ]).then(function () {
         liff.closeWindow();
     }).catch(function (error) {
@@ -194,4 +219,8 @@ function sendMessage(){
     }).catch(function (error) {
         alert("Failed to launch ShareTargetPicker : "+ error.message)
     });
+}
+
+function setting(){
+    location = "/setting.html";
 }
